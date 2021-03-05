33c3e73bc0e1b4c238cef284008829673ee1463d
wujimaster <wujiwust@gmail.com>
2021-03-05 17:04:18 +0800

    /proc/loadavg

--- entity/sysinfo_check.py
+++ entity/sysinfo_check.py
@@ -19,7 +19,7 @@ class Disk(object):
         self.get_disk()
 
     def __str__(self):
-        return 'disk status: ' + '\nsize: ' + str(self.size) + '\nused: ' + str(self.used) + '\navail:' + str(self.avail) + '\npercent:' + str(self.percent)
+        return 'disk status: ' + '\nsize: ' + str(self.size) + '\nused: ' + str(self.used) + '\navail: ' + str(self.avail) + '\npercent: ' + str(self.percent) + ' %'
 
     # initialize data
     def get_disk(self):
@@ -40,6 +40,7 @@ class Disk(object):
 
 class Processor(object):
     def __init__(self):
+        self.loadavg_path = '/proc/loadavg'
         self.cpu_count = cpu_count()
         self.upload = {"1m": 0.0, "5m": 0.0, "15m": 0.0}
         self.percent = float()
@@ -50,8 +51,25 @@ class Processor(object):
     def __str__(self):
         return "\nupload is : " + str(self.upload) + "\ncpu usage: " + str(self.percent) + " %"
 
-    # ReturnType dict
+    # ReturnType: dict
     def get_upload(self):
+        try:
+            if not os.path.isfile(self.loadavg_path):
+                raise Exception('{} does not exist.'.format(self.loadavg_path))
+            with open(self.loadavg_path, 'r') as f:
+                result = f.readline()
+                upload = result.split()
+                self.upload["1m"] = float(upload[0])
+                self.upload["5m"] = float(upload[1])
+                self.upload["15m"] = float(upload[2])
+                assert type(self.upload) == dict
+                return self.upload
+        except Exception as e:
+            logger.error(traceback.format_exc())
+
+    # status: DEPRECATED
+    # ReturnType dict
+    def get_upload_by_uptime(self):
         try:
             result = subprocess.Popen("uptime", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             upload = result.stdout.readline().strip("\n").split(":")[-1].split(",")
@@ -79,8 +97,6 @@ class Processor(object):
 class Memory(object):
     def __init__(self):
         self.mem_info_path = '/proc/meminfo'
-        if not os.path.isfile(self.mem_info_path):
-            raise Exception('{} does not exist.'.format(self.mem_info_path))
         self.total = 0
         self.free = 0
         self.avail = 0
@@ -90,6 +106,8 @@ class Memory(object):
         self.get_mem_info()
 
     def get_mem_info(self):
+        if not os.path.isfile(self.mem_info_path):
+            raise Exception('{} does not exist.'.format(self.mem_info_path))
         try:
             with open(self.mem_info_path, 'r') as f:
                 lines = f.readlines()
